f736b4fba366f5788285e01b7a757b4655b23371
NoSparrow <wroblewski.m@int.pl>
2024-06-29 12:45:43 +0200

    Zrobiony popup

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,14 +1,4 @@
-
-// setTimeout(() => {
-    
-// }, timeout);
-
-
-
-
-
 setTimeout(function showPopup() {
-	document.getElementById("card").style.display = "none";
 	document.getElementById("popup").style.display = "block";
 }, 1000);
 
@@ -20,8 +10,3 @@ function disablePopup() {
 	document.getElementById("popup").style.display = "none";
 	document.getElementById("card").style.display = "block";
 }
-
-// 1. Animacja znikania strony głównej
-// 2. Animacja pojawiania się popup-u
-// 3. Animacja znikania popup-u
-// 4. Animacja pojawiania się strony głównej
